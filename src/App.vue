d74100926e16ef9c22568202baabff4691ec3b62
张天 <zhangtian@zhangtiandeMacBook-Air.local>
2018-12-12 16:43:13 +0800

    xxx

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     <!-- Demo1 -->
 		<div v-show="showDemoPart.demo1Show">
 			<div v-show="!dateDemo1.showChild">
-				<h2>自动生成基础信息(父->子)</h2>
+				<h2>自动生成基础信息(父->子)按撒娇说</h2>
         <span>我是女娲，我要捏一个子辈</span>
 				<el-row>
           <span>xxx11</span>  
